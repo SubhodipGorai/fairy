afd2c3fa2b1f9fc72039e5e1a869d995801937b1
SubhodipGorai <75923956+SubhodipGorai@users.noreply.github.com>
2021-02-21 15:23:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,66 @@
-var canvas;
-var music;
-var ball,a,b,c,d,edge;
-function preload(){
-    music = loadSound("music.mp3");
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
 }
 
+function setup() {
+	createCanvas(800, 750);
 
-function setup(){
-    canvas = createCanvas(800,600);
-    //create 4 different surfaces
-    a = createSprite(100,550,200,20); 
-    a.shapeColor = "red";
-    b = createSprite(300,550,200,20); 
-    b.shapeColor = "blue";
-    c = createSprite(500,550,200,20); 
-    c.shapeColor = "green";
-    d = createSprite(700,550,200,20); 
-    d.shapeColor = "yellow";
+	// fairyVoice.play();
 
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
 
-    //create box sprite and give velocity
-    ball = createSprite(400,50,20,20); 
-    ball.velocityX = -2;
-    ball.velocityY = 3;
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
 }
+
 
 function draw() {
-    background(0);
-    bounceoff();
-    createEdgeSprites();
-    if(a.isTouching(ball)){
-        ball.shapeColor ="red";
-    }
-    if(b.isTouching(ball)){
-        ball.shapeColor ="blue";
-    }
-    if(c.isTouching(ball)){
-        ball.shapeColor ="green";
-    }
-    if(d.isTouching(ball)){
-        ball.shapeColor ="yellow";
-    }
-    drawSprites();
+  background(bgImg);
+  keyPressed();
+  drawSprites();
+
 }
-function bounceoff() {
-    ball.bounceOff(a);
-    ball.bounceOff(b);
-    ball.bounceOff(c);
-    ball.bounceOff(d);
-    ball.bounceOff(edge);
+
+function keyPressed() {
+	if (keyWentDown("leftArrow")) {
+		fairy.x = fairy.x-5;
+		
+	}
+	if (keyWentDown("rightArrow")) {
+		fairy.x = fairy.x+5;
+		
+	}
+	if (keyDown("space")) {
+		starBody.velocityY = 3;
+	}
+	if (starBody.isTouching(fairy)) {
+		starBody.velocityY = 0;
+		starBody.velocityX = 0;
+	}
 }
